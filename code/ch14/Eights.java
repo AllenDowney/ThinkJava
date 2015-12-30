3d5a54860dca96d539731e8663c7ce651ba9aa21
Chris Mayfield <mayfiecs@jmu.edu>
2015-12-29 23:08:49 -0500

    split Deck2 into Player and Pile

--- code/ch14/Eights.java
+++ code/ch14/Eights.java
@@ -23,11 +23,13 @@ public class Eights {
         
         // remaining cards are placed face down
         // and the top card is turned face up
-        Deck pack = deck.subdeck(16, 50);
-        Deck disc = deck.subdeck(51, 51);
+        Pile pack = new Pile("Draw Pile");
+        pack.deal(deck.subdeck(16, 50));
+        Pile disc = new Pile("Discard Pile");
+        disc.deal(deck.subdeck(51, 51));
         
         // keep playing until there's a winner
-        Player player = one;
+        Player current = one;
         while (!one.empty() && !two.empty()) {
             
             // output the current state of the game
@@ -40,33 +42,31 @@ public class Eights {
             System.out.println(disc);
             in.nextLine();
             
-            // play the next card
+            // play the next card, drawing cards if needed
             Card prev = disc.last();
-            Card next = player.play(prev);
+            Card next = current.play(prev);
             while (next == null) {
                 if (pack.empty()) {
-                    // replace the pack with discard pile
-                    Deck temp = pack;
-                    pack = disc;
-                    disc = temp;
-                    // move the top card back into discard
-                    pack.remove(prev);
+                    System.out.println("shuffling discard pile into draw pile");
+                    pack.swap(disc);
+                    prev = pack.remove();
                     disc.add(prev);
                     pack.shuffle();
                 }
-                System.out.println(player.getName() + " draws " + pack.first());
-                player.draw(pack);
-                next = player.play(prev);
+                Card draw = pack.remove();
+                System.out.println(current.getName() + " draws " + draw);
+                current.add(draw);
+                next = current.play(prev);
             }
-            System.out.println(player.getName() + " plays " + next);
+            System.out.println(current.getName() + " plays " + next);
             System.out.println();
             disc.add(next);
             
-            // pick the next player
-            if (player == one) {
-                player = two;
+            // select the next player
+            if (current == one) {
+                current = two;
             } else {
-                player = one;
+                current = one;
             }
         }
         
