56563c1ac62ec5baba88a972d693454face721cf
Chris Mayfield <mayfiecs@jmu.edu>
2016-01-07 14:24:35 -0600

    read through ch1

--- split.py
+++ split.py
@@ -2,13 +2,13 @@
 
 import sys
 
-def main(n):
-    src = open("thinkjava.tex")
+def main(tex, num):
+    src = open(tex)
     # copy the preamble
     for line in src:
         print line,
         if line.startswith("\\begin{document}"):
-            print "\\setcounter{chapter}{" + str(n-1) + "}\n"
+            print "\\setcounter{chapter}{" + str(num - 1) + "}\n"
             break
     # copy the chapter
     i = 0
@@ -17,14 +17,14 @@ def main(n):
             break
         if line.startswith("\\chapter{"):
             i += 1
-            if i > n:
+            if i > num:
                 break
-        if i == n:
+        if i == num:
             print line,
     print "\\end{document}"
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main(int(sys.argv[1]))
+    if len(sys.argv) == 3:
+        main(sys.argv[1], int(sys.argv[2]))
     else:
-        print "Usage: python chapter.py NUMBER"
+        print "Usage: python split.py MAIN.tex NUMBER"
