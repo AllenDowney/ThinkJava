fb0ec27eadef0fdcdbd34e6b7f6b7329e114b0ae
Allen Downey <downey@allendowney.com>
2016-01-12 14:49:18 -0500

    Adding ch10 code

--- code/ch11/Time.java
+++ code/ch11/Time.java
@@ -98,7 +98,10 @@ public class Time {
             this.hour += 1;
         }
     }
-    
+
+    /**
+     * Test the methods in this class.
+     */
     public static void main(String[] args) {
         Time time = new Time(11, 59, 59.9);
         System.out.println(time);
