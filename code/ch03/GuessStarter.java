9326138506c51a7da41f83ef0dd924a8e8cad121
Allen Downey <downey@allendowney.com>
2016-01-12 11:10:27 -0500

    Cleaning ch03 code

--- code/ch03/GuessStarter.java
+++ code/ch03/GuessStarter.java
@@ -1,12 +1,14 @@
 import java.util.Random;
 
-
+/**
+ * Starter for for the "guess my number" exercise.
+ */
 public class GuessStarter {
-
+    
     public static void main(String[] args) {
-	// pick a random number
-	Random random = new Random();
-	int number = random.nextInt(100) + 1;
+        // pick a random number
+        Random random = new Random();
+        int number = random.nextInt(100) + 1;
         System.out.println(number);
     }
 }
