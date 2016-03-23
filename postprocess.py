a44a52777327846d143426513880ca372ab4b551
Allen Downey <downey@allendowney.com>
2016-03-23 13:34:37 -0400

    Keeping email address together

--- postprocess.py
+++ postprocess.py
@@ -6,9 +6,15 @@ def main(name, filename, *argv):
     # print the contents of the given file
     ft = Filist(filename)
     ft.sub_lines(r'<programlisting>java', r'<programlisting language="java">')
+
+    # label the last three chapters as appendices
     i, match = ft.search_lines('<chapter id="development">')
     ft.sub_lines(r'<chapter', r'<appendix', start=i)
     ft.sub_lines(r'</chapter', r'</appendix', start=i+1)
+
+    ft.sub_lines(r'<emphasis role="bold">feedback@greenteapress.com</emphasis>',
+                 r'<phrase role="keep-together"><emphasis role="bold">feedback@greenteapress.com</emphasis></phrase>')
+
     print ft
 
 if __name__ == '__main__':
