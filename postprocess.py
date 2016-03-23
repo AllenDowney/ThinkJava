9e66c72c531a462677918eaaef8d38b09e53494a
Allen Downey <downey@allendowney.com>
2016-03-23 13:15:02 -0400

    Labeling appendices

--- postprocess.py
+++ postprocess.py
@@ -6,6 +6,9 @@ def main(name, filename, *argv):
     # print the contents of the given file
     ft = Filist(filename)
     ft.sub_lines(r'<programlisting>java', r'<programlisting language="java">')
+    ft.sub_lines(r'<chapter id="development">', r'<appendix id="development">')
+    ft.sub_lines(r'<chapter id="graphics">', r'<appendix id="graphics">')
+    ft.sub_lines(r'<chapter id="debugappendix">', r'<appendix id="debugappendix">')
     print ft
 
 if __name__ == '__main__':
